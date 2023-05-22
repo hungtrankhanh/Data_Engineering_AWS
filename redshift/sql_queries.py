7d471166b9a5da0a4103679ebd35396f3b3061d6
Hung <hung.trankhanh09@gmail.com>
2023-05-22 09:37:02 +0800

    updated redshift

--- redshift/sql_queries.py
+++ redshift/sql_queries.py
@@ -1,272 +1,270 @@
-import configparser
+<!DOCTYPE HTML>
+<html>
 
+<head>
+    <meta charset="utf-8">
 
-# CONFIG
-config = configparser.ConfigParser()
-config.read('dwh.cfg')
+    <title>sql_queries.py (editing)</title>
+    <link id="favicon" rel="shortcut icon" type="image/x-icon" href="/static/base/images/favicon-file.ico?v=e2776a7f45692c839d6eea7d7ff6f3b2">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="/static/components/jquery-ui/themes/smoothness/jquery-ui.min.css?v=3c2a865c832a1322285c55c6ed99abb2" type="text/css" />
+    <link rel="stylesheet" href="/static/components/jquery-typeahead/dist/jquery.typeahead.min.css?v=7afb461de36accb1aa133a1710f5bc56" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+<link rel="stylesheet" href="/static/components/codemirror/lib/codemirror.css?v=288352df06a67ee35003b0981da414ac">
+<link rel="stylesheet" href="/static/components/codemirror/addon/dialog/dialog.css?v=c89dce10b44d2882a024e7befc2b63f5">
 
-# DROP TABLES
-
-staging_events_table_drop = "DROP TABLE IF EXISTS staging_events_data"
-staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs_data"
-songplay_table_drop = "DROP TABLE IF EXISTS song_plays"
-user_table_drop = "DROP TABLE IF EXISTS users"
-song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS time"
-
-# CREATE TABLES
-
-staging_events_table_create= ("""
-    CREATE TABlE staging_events_data ( 
-        event_id int IDENTITY(0,1), 
-        artist varchar(255), 
-        auth varchar(255),  
-        firstName varchar(255), 
-        gender varchar(2), 
-        itemInSession int, 
-        lastName varchar(255), 
-        length double precision, 
-        level varchar(10), 
-        location varchar(255), 
-        method varchar(10), 
-        page varchar(20), 
-        registration varchar(50), 
-        sessionId int, 
-        song varchar(255), 
-        status smallint, 
-        ts bigint, 
-        userAgent varchar(255), 
-        userId int)
-    """)
-
-staging_songs_table_create = ("""
-    CREATE TABlE staging_songs_data ( 
-        num_songs int, 
-        artist_id varchar(50), 
-        artist_latitude double precision, 
-        artist_longitude double precision, 
-        artist_location varchar(255), 
-        artist_name varchar(255), 
-        song_id varchar(50), 
-        title varchar(255), 
-        duration double precision, 
-        year smallint ) 
-    """)
+    <link rel="stylesheet" href="/static/style/style.min.css?v=4b4b8cb1e49605137f77fed041f8922b" type="text/css"/>
     
 
-songplay_table_create = ("""
-    CREATE TABlE song_plays ( 
-        songplay_id int IDENTITY(0,1) PRIMARY KEY sortkey, 
-        start_time datetime NOT NULL REFERENCES time(start_time), 
-        user_id int NOT NULL REFERENCES users(user_id) distkey, 
-        level varchar(10), 
-        song_id varchar(50) NOT NULL REFERENCES songs(song_id), 
-        artist_id varchar(50) NOT NULL REFERENCES artists(artist_id), 
-        session_id int , 
-        location varchar(255), 
-        user_agent varchar(255) 
-    )
-    """)
+    <link rel="stylesheet" href="/custom/custom.css" type="text/css" />
+    <script src="/static/components/es6-promise/promise.min.js?v=f004a16cb856e0ff11781d01ec5ca8fe" type="text/javascript" charset="utf-8"></script>
+    <script src="/static/components/preact/index.js?v=00a2fac73c670ce39ac53d26640eb542" type="text/javascript"></script>
+    <script src="/static/components/proptypes/index.js?v=c40890eb04df9811fcc4d47e53a29604" type="text/javascript"></script>
+    <script src="/static/components/preact-compat/index.js?v=aea8f6660e54b18ace8d84a9b9654c1c" type="text/javascript"></script>
+    <script src="/static/components/requirejs/require.js?v=951f856e81496aaeec2e71a1c2c0d51f" type="text/javascript" charset="utf-8"></script>
+    <script>
+      require.config({
+          
+          urlArgs: "v=20230522013257",
+          
+          baseUrl: '/static/',
+          paths: {
+            'auth/js/main': 'auth/js/main.min',
+            custom : '/custom',
+            nbextensions : '/nbextensions',
+            kernelspecs : '/kernelspecs',
+            underscore : 'components/underscore/underscore-min',
+            backbone : 'components/backbone/backbone-min',
+            jed: 'components/jed/jed',
+            jquery: 'components/jquery/jquery.min',
+            json: 'components/requirejs-plugins/src/json',
+            text: 'components/requirejs-text/text',
+            bootstrap: 'components/bootstrap/js/bootstrap.min',
+            bootstraptour: 'components/bootstrap-tour/build/js/bootstrap-tour.min',
+            'jquery-ui': 'components/jquery-ui/jquery-ui.min',
+            moment: 'components/moment/min/moment-with-locales',
+            codemirror: 'components/codemirror',
+            termjs: 'components/xterm.js/xterm',
+            typeahead: 'components/jquery-typeahead/dist/jquery.typeahead.min',
+          },
+          map: { // for backward compatibility
+              "*": {
+                  "jqueryui": "jquery-ui",
+              }
+          },
+          shim: {
+            typeahead: {
+              deps: ["jquery"],
+              exports: "typeahead"
+            },
+            underscore: {
+              exports: '_'
+            },
+            backbone: {
+              deps: ["underscore", "jquery"],
+              exports: "Backbone"
+            },
+            bootstrap: {
+              deps: ["jquery"],
+              exports: "bootstrap"
+            },
+            bootstraptour: {
+              deps: ["bootstrap"],
+              exports: "Tour"
+            },
+            "jquery-ui": {
+              deps: ["jquery"],
+              exports: "$"
+            }
+          },
+          waitSeconds: 30,
+      });
 
-user_table_create = ("""
-    CREATE TABlE users ( 
-        user_id int PRIMARY KEY sortkey distkey, 
-        first_name varchar(255) NOT NULL, 
-        last_name varchar(255) NOT NULL, 
-        gender varchar(2), 
-        level varchar(10)) 
-     """)
+      require.config({
+          map: {
+              '*':{
+                'contents': 'services/contents',
+              }
+          }
+      });
 
-song_table_create = ("""
-    CREATE TABLE songs ( 
-        song_id varchar(50) PRIMARY KEY distkey, 
-        title varchar(255), 
-        artist_id varchar(50), 
-        year int, 
-        duration double precision) 
-     """)
+      // error-catching custom.js shim.
+      define("custom", function (require, exports, module) {
+          try {
+              var custom = require('custom/custom');
+              console.debug('loaded custom.js');
+              return custom;
+          } catch (e) {
+              console.error("error loading custom.js", e);
+              return {};
+          }
+      })
 
-artist_table_create = ("""
-    CREATE TABLE artists ( 
-        artist_id varchar(50) PRIMARY KEY distkey, 
-        name varchar(255) , 
-        location varchar(255), 
-        latitude double precision, 
-        longitude double precision)
-    """)
+    document.nbjs_translations = {"domain": "nbjs", "locale_data": {"nbjs": {"": {"domain": "nbjs"}}}};
+    document.documentElement.lang = navigator.language.toLowerCase();
+    </script>
 
-time_table_create = ("""
-    CREATE TABLE time ( 
-        start_time datetime PRIMARY KEY, 
-        hour smallint NOT NULL, 
-        day smallint NOT NULL, 
-        week smallint NOT NULL, 
-        month smallint NOT NULL, 
-        year smallint NOT NULL, 
-        weekday smallint NOT NULL) 
-    """)
-
-# STAGING TABLES
-
-staging_events_copy = ("""
-    copy  staging_events_data from {}
-    credentials 'aws_iam_role={}' 
-    format as json {} compupdate off region 'us-west-2';
-""").format(config.get("S3","LOG_DATA"), config.get("IAM_ROLE","ARN"), config.get("S3","LOG_JSONPATH"))
-
-staging_songs_copy = ("""
-     copy  staging_songs_data from {}
-     credentials 'aws_iam_role={}' 
-     json 'auto' compupdate off region 'us-west-2';
-""").format(config.get("S3","SONG_DATA"), config.get("IAM_ROLE","ARN"))
-
-# FINAL TABLES
-
-songplay_table_insert = ("""
-    INSERT INTO song_plays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-    SELECT DISTINCT
-        date_add('ms',e.ts,'1970-01-01') as start_time,
-        e.userId as user_id,
-        e.level as level,
-        s.song_id as song_id,
-        s.artist_id as artist_id,
-        e.sessionId as session_id,
-        e.location as location,
-        e.userAgent as user_agent
-    FROM staging_events_data e 
-    JOIN staging_songs_data s ON (e.song = s.title AND e.length=s.duration)
-
-""")
-
-user_table_insert = ("""
-    INSERT INTO users (user_id, first_name, last_name, gender, level)
-    SELECT DISTINCT 
-        e.userId as user_id,
-        e.firstName as first_name,
-        e.lastName as last_name,
-        e.gender as gender,
-        e.level as level
-    FROM staging_events_data e
-    WHERE user_id is not NULL
     
-""")
+    
 
-song_table_insert = ("""
-    INSERT INTO songs (song_id, title, year, artist_id, duration)
-    SELECT DISTINCT
-        s.song_id as song_id,
-        s.title as title,
-        s.year as year,
-        s.artist_id as artist_id,
-        s.duration as duration
-    FROM staging_songs_data s
-""")
+</head>
 
-artist_table_insert = ("""
-    INSERT INTO artists (artist_id, name, location, latitude, longitude)
-    SELECT DISTINCT
-        s.artist_id as artist_id,
-        s.artist_name as name,
-        s.artist_location as location,
-        s.artist_latitude as latitude,
-        s.artist_longitude as longitude
-    FROM staging_songs_data s
-""")
+<body class="edit_app "
+ 
+data-base-url="/"
+data-file-path="sql_queries.py"
 
-time_table_insert = ("""
-    INSERT INTO time(start_time, hour, day, week, month, year, weekday)
-    SELECT DISTINCT
-         date_add('ms',e.ts,'1970-01-01') as start_time,
-         EXTRACT(hour FROM start_time)   as hour,
-         EXTRACT(day FROM start_time)  as day,
-         EXTRACT(week FROM start_time) as week,
-         EXTRACT(month FROM start_time) as month,
-         EXTRACT(year FROM start_time) as year,
-         EXTRACT(weekday FROM start_time) as weekday
-    FROM staging_events_data e
-""")
+  
+ 
+
+dir="ltr">
+
+<noscript>
+    <div id='noscript'>
+      Jupyter Notebook requires JavaScript.<br>
+      Please enable it to proceed. 
+  </div>
+</noscript>
+
+<div id="header">
+  <div id="header-container" class="container">
+  <div id="ipython_notebook" class="nav navbar-brand"><a href="/tree" title='dashboard'>
+      <img src='/static/base/images/logo.png?v=641991992878ee24c6f3826e81054a0f' alt='Jupyter Notebook'/>
+  </a></div>
+
+  
+
+<span id="save_widget" class="pull-left save_widget">
+    <span class="filename"></span>
+    <span class="last_modified"></span>
+</span>
 
 
-## QUERY INSIGHT FROM ANALYSED TABLES
+  
+  
+  
+  
 
-query_session_with_num_of_user = """
-    SELECT
-        t1.session_id as session_id ,
-        count(*) as num_of_user
-    FROM song_plays t1
-    JOIN users t2 ON (t1.user_id=t2.user_id)
-    GROUP BY  t1.session_id
-    ORDER BY num_of_user desc
-    LIMIT 10
-"""
+    <span id="login_widget">
+      
+    </span>
 
-query_on_song_artist_location = """
-    SELECT
-        t3.title as song_title,
-        t4.name as artist_name,
-        t4.location as location,
-        count(t2.user_id) as num_of_user
-    FROM song_plays t1
-    JOIN users t2 on (t1.user_id=t2.user_id)
-    JOIN songs t3 on (t1.song_id=t3.song_id)
-    JOIN artists t4 on (t1.artist_id=t4.artist_id)
-    GROUP BY t3.title, t4.name, t4.location
-    ORDER BY num_of_user desc
-    LIMIT 10 
-"""
+  
 
-query_on_song_artist_latitude = """
-    SELECT
-        t3.title as song_title,
-        t4.name as artist_name,
-        t4.latitude as latitude,
-        count(t2.user_id) as num_of_user
-    FROM song_plays t1
-    JOIN users t2 on (t1.user_id=t2.user_id)
-    JOIN songs t3 on (t1.song_id=t3.song_id)
-    JOIN artists t4 on (t1.artist_id=t4.artist_id)
-    GROUP BY t3.title, t4.name, t4.latitude
-    ORDER BY num_of_user desc
-    LIMIT 10 
-"""
+  
+  
+  </div>
+  <div class="header-bar"></div>
 
-query_on_song_artist_location_with_female = """
-    SELECT
-        t3.title as song_title,
-        t4.name as artist_name,
-        t4.location as location,
-        count(t2.user_id) as num_of_user
-    FROM song_plays t1
-    JOIN users t2 on (t1.user_id=t2.user_id)
-    JOIN songs t3 on (t1.song_id=t3.song_id)
-    JOIN artists t4 on (t1.artist_id=t4.artist_id)
-    WHERE t2.gender ='F'
-    GROUP BY t3.title, t4.name, t4.location
-    ORDER BY num_of_user desc
-    LIMIT 10 
-"""
+  
 
-query_on_song_artist_location_with_female_paid = """
-    SELECT
-        t3.title as song_title,
-        t4.name as artist_name,
-        t4.location as location,
-        count(t2.user_id) as num_of_user
-    FROM song_plays t1
-    JOIN users t2 on (t1.user_id=t2.user_id)
-    JOIN songs t3 on (t1.song_id=t3.song_id)
-    JOIN artists t4 on (t1.artist_id=t4.artist_id)
-    JOIN time t5 on (t1.start_time=t5.start_time)
-    WHERE t2.gender='F' AND t2.level='paid'
-    GROUP BY t3.title, t4.name, t4.location
-    ORDER BY num_of_user desc
-    LIMIT 10 
-"""
+<div id="menubar-container" class="container">
+  <div id="menubar">
+    <div id="menus" class="navbar navbar-default" role="navigation">
+      <div class="container-fluid">
+          <p  class="navbar-text indicator_area">
+          <span id="current-mode" >current mode</span>
+          </p>
+        <button type="button" class="btn btn-default navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <i class="fa fa-bars"></i>
+          <span class="navbar-text">Menu</span>
+        </button>
+        <ul class="nav navbar-nav navbar-right">
+          <li id="notification_area"></li>
+        </ul>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">File</a>
+              <ul id="file-menu" class="dropdown-menu">
+                <li id="new-file"><a href="#">New</a></li>
+                <li id="save-file"><a href="#">Save</a></li>
+                <li id="rename-file"><a href="#">Rename</a></li>
+                <li id="download-file"><a href="#">Download</a></li>
+              </ul>
+            </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Edit</a>
+              <ul id="edit-menu" class="dropdown-menu">
+                <li id="menu-find"><a href="#">Find</a></li>
+                <li id="menu-replace"><a href="#">Find &amp; Replace</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Key Map</li>
+                <li id="menu-keymap-default"><a href="#">Default<i class="fa"></i></a></li>
+                <li id="menu-keymap-sublime"><a href="#">Sublime Text<i class="fa"></i></a></li>
+                <li id="menu-keymap-vim"><a href="#">Vim<i class="fa"></i></a></li>
+                <li id="menu-keymap-emacs"><a href="#">emacs<i class="fa"></i></a></li>
+              </ul>
+            </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">View</a>
+              <ul id="view-menu" class="dropdown-menu">
+              <li id="toggle_header" title="Show/Hide the logo and notebook title (above menu bar)">
+              <a href="#">Toggle Header</a></li>
+              <li id="menu-line-numbers"><a href="#">Toggle Line Numbers</a></li>
+              </ul>
+            </li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Language</a>
+              <ul id="mode-menu" class="dropdown-menu">
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-# QUERY LISTS
+<div class="lower-header-bar"></div>
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-copy_table_queries = [staging_events_copy, staging_songs_copy]
-insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+
+</div>
+
+<div id="site">
+
+
+<div id="texteditor-backdrop">
+<div id="texteditor-container" class="container"></div>
+</div>
+
+
+</div>
+
+
+
+
+
+
+    
+
+
+<script src="/static/edit/js/main.min.js?v=ac978ed627b00fbfa328acc3f14b22fb" type="text/javascript" charset="utf-8"></script>
+
+
+<script type='text/javascript'>
+  function _remove_token_from_url() {
+    if (window.location.search.length <= 1) {
+      return;
+    }
+    var search_parameters = window.location.search.slice(1).split('&');
+    for (var i = 0; i < search_parameters.length; i++) {
+      if (search_parameters[i].split('=')[0] === 'token') {
+        // remote token from search parameters
+        search_parameters.splice(i, 1);
+        var new_search = '';
+        if (search_parameters.length) {
+          new_search = '?' + search_parameters.join('&');
+        }
+        var new_url = window.location.origin + 
+                      window.location.pathname + 
+                      new_search + 
+                      window.location.hash;
+        window.history.replaceState({}, "", new_url);
+        return;
+      }
+    }
+  }
+  _remove_token_from_url();
+</script>
+</body>
+
+</html>
